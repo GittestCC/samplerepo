7478d373a77dc8658105b0452b2e3fa6556c50dc
Nadeem Khedr <nadeemkhedr@gmail.com>
2018-03-15 16:59:48 +0800

    update listen to host, add a sample request

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ import Hapi from 'hapi'
 
 // Create a server with a host and port
 const server = Hapi.server({
-  host: 'localhost',
+  host: '127.0.0.1',
   port: process.env.PORT || '8000'
 })
 
@@ -16,6 +16,24 @@ const server = Hapi.server({
 server.route({
   method: 'GET',
   path: '/',
+  handler: async function(request, h) {
+    return h.response({
+      data: 'test data',
+      requestHeaders: request.headers
+    }).header('kbsampleprovide-account-id', 'freakinyes')
+  }
+})
+
+/**
+ * @api {get} /sample get protocol docs
+ * @apiName GetDetails
+ * @apiSuccess (Success_200) {Object} requestHeaders the request headers sent
+ * @apiSuccess (Success_200) {String} data test text
+ * @apiSuccess (Session) {String} kbsampleprovide-account-id testing the dependency headers
+ */
+server.route({
+  method: 'GET',
+  path: '/sample',
   handler: async function(request, h) {
     return h.response({
       data: 'test data',
